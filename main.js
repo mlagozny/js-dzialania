99a2cbee26c881abc50a7881f94e15ef9f0acab1
mlagozny <mateusz_lagozny@o2.pl>
2020-07-15 11:57:36 +0200

    js-działania

---
+++ main.js
@@ -0,0 +1,23 @@
+function mathsOperations (num1, num2) {
+
+
+    if ((num1 + num2) > 0) {
+        console.log ('Wynik dodawania wynosi' + result1);
+    } else { 
+        console.log('Wynik jest nieprawidłowy');
+    }
+
+    if ((num1 - num2) > 0 ) {
+        console.log ('Wynik odejmowania wynosi' + result2);
+    } else {
+        console.log('Wynik jest nieprawidłowy'); 
+    }
+ 
+    if ((num1 * num2) > 0) {
+        console.log ('Wynik mnożenia wynosi' + result3);
+    } else {
+        console.log('Wynik jest nie prawdłowy');
+    }
+}
+
+mathsOperations(5, 1)
